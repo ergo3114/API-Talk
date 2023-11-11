ac2f7fda55d1646067a65406f61a78e988f887d9
C. David Littlejohn <cdlittlejohn1@gmail.com>
2023-11-10 20:25:16 -0500

    POST url API and new endpoint

--- 1-RESTAPI.ps1
+++ 1-RESTAPI.ps1
@@ -2,14 +2,19 @@ return 'This is a demo and should be run as such'
 
 # URLs
 $urlCatFacts = 'https://cat-fact.herokuapp.com/facts/'
-$urlPokeMon = 'https://pokeapi.co/api/v2/ditto/'
+$urlPokeMon = 'https://pokeapi.co/api/v2/pokemon/6/'
 
 # Call uri using the GET method to get data and put it into a variable
 $objCatFacts = Invoke-RestMethod -Uri $urlCatFacts -Method 'GET'
 
 # See what we have
 $objCatFacts | Get-Member
+$objCatFacts.Count
 $objCatFacts.text
 
+# Another call uri using the GET method to perform an action on an parameter
+$objPokemonFacts = Invoke-RestMethod -Uri $urlPokeMon -Method 'GET'
+
+# https://sqa.stackexchange.com/questions/47097/free-sites-for-testing-post-rest-api-calls
 # Call uri using the POST method to perform an action on an parameter
-$objCatFacts = Invoke-RestMethod -Uri $urlPokeMon -Method 'GET'
+Invoke-RestMethod https://httpbin.org/post -Method Post -Body "Testing"
